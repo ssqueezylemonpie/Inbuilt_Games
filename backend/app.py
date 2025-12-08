132779dc4dad34da68e14179469eee2b09d60e7e
ssqueezylemonpie <yerom2200@gmail.com>
2025-12-08 20:51:19 +0100

    update

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,13 @@ import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+# Get the parent directory (project root)
+basedir = os.path.abspath(os.path.dirname(__file__))
+parent_dir = os.path.dirname(basedir)
+
 app = Flask(__name__,
-            template_folder='templates',
-            static_folder='static')
+            template_folder=os.path.join(parent_dir, 'templates'),
+            static_folder=os.path.join(parent_dir, 'static'))
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -12,6 +16,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
 
 @app.route('/')
 def home():
@@ -28,3 +35,9 @@ def match():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+with app.app_context():
+    db.create_all()  
